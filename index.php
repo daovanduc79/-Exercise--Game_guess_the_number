bc252995c11c677f40fb1fa3957ff846aa814566
daovanduc <duc07092000@gmail.com>
2020-04-26 23:52:20 +0700

    update index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Game doan so</title>
+</head>
+<body>
+<form action="action.php" method="post">
+    <label>Nhập vào số bạn muốn (1->100):<input type="number" name="needle"></label>
+</form>
+
+</body>
+</html>
